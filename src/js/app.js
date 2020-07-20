4b11829423f0183e088b33c951a9a4905940bb34
Telat Kaya <telatkaya@yandex.com>
2020-07-21 00:50:33 +0300

    app landing page design to semantic

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
-$(document)
-		.ready(function () {
-				'use strict';
-				// =require pages/_index.js
-				// =require base/_theme.js
-				// =require partials/_metisMenu.js
-				// =require components/_charts-resize.js
-				// =require components/_toggle-sidebar.js
-				// =require components/_navbar.js
-
-		});
+$(document).ready(function() {
+	'use strict';
+	// =require pages/_index.js
+	// =require base/_theme.js
+	// =require partials/_metisMenu.js
+	// =require components/_charts-resize.js
+	// =require components/_toggle-sidebar.js
+	// =require components/_navbar.js
+});
